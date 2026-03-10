16244e797c1566f8a2ea0ba4fab5af5bb0801dbc
Nathalia Machado <n4th.machado@gmail.com>
2026-03-09 23:42:15 -0300

    feat: Inicializa o Projeto

---
+++ momo-ecommerce/src/main/java/com/momo/ecommerce/MoMoECommerceApplication.java
@@ -0,0 +1,13 @@
+package com.momo.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoMoECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoMoECommerceApplication.class, args);
+	}
+
+}
---
+++ momo-ecommerce/src/test/java/com/momo/ecommerce/MoMoECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.momo.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoMoECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
