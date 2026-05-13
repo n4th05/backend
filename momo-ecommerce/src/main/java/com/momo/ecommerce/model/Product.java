b9281b7d6a91d2b07a76281bbdba3bdd3132e528
Nathalia Machado <n4th.machado@gmail.com>
2026-05-13 19:39:23 -0300

    feat: Criar entidade Product

--- momo-ecommerce/src/main/java/com/momo/ecommerce/model/Product.java
+++ momo-ecommerce/src/main/java/com/momo/ecommerce/model/Product.java
@@ -1,37 +1,72 @@
 package com.momo.ecommerce.model;
 
+import java.time.LocalDateTime;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "products")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "Nome é obrigatório.")
+    @Size(min = 3, max = 100, message = "Nome deve ter entre 3 ou 100 caracteres.")
+    @Column(nullable = false, length = 100)
     private String name;
+
+    @Size(max = 500, message = "Descrição deve ter no máximo 500 caracteres.")
+    @Column(length = 500)
+    private String description;
+
+    @NotBlank(message = "Preço é obrigatório.")
+    @Positive(message = "Preço deve ser maior que zero.")
+    @Column(nullable = false, precision = 10, scale = 2)
     private Double price;
 
-    public Long getId() {
-        return id;
+    @NotNull(message = "Quantidade em estoque é obrigadatória.")
+    @Min(value = 0, message = "Quantidade não pode ser negativa.")
+    @Column(nullable = false)
+    private Integer StockQuantity;
+
+    @Column(nullable = false)
+    private Boolean active = true;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
     }
-    public void setId(Long id) {
-        this.id = id;
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
     }
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public Double getPrice() {
-        return price;
-    }
-    public void setPrice(Double price) {
-        this.price = price;
-    }    
 }
