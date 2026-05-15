1c0e682bd77510dd88bb341d87afda8ca2b6c21c
Nathalia Machado <n4th.machado@gmail.com>
2026-05-15 16:24:58 -0300

    feat: Criar Entidade de Category

---
+++ momo-ecommerce/src/main/java/com/momo/ecommerce/model/Category.java
@@ -0,0 +1,74 @@
+package com.momo.ecommerce.model;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "categories")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Category {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    @Size(min = 3, max = 50, message = "Nome deve ter entre 3 e 50 caracteres.")
+    @NotBlank(message = "Nome é obrigatório.")
+    private String name;
+
+    @Column(nullable = false, unique = true, length = 500)
+    @Size(min = 10, max = 500, message = "Descrição deve ter entre 10 ou 500 caracteres.")
+    @NotBlank(message = "Descrição é obrigatório.")
+    private String description;
+
+
+    @OneToMany(mappedBy = "category", fetch = FetchType.LAZY)
+    @JsonIgnore
+    @Builder.Default
+    private List<Product> products = new ArrayList<>();
+
+    @Column(nullable = false)
+    @Builder.Default
+    private Boolean active = true;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+}
